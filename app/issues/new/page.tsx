1280b505a28136dc354679d9cbaa985ae4f91349
Prajwal Neupane <prajwalneupane.981@gmail.com>
2024-01-10 16:43:09 +0545

    form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,21 +1,62 @@
 "use client";
 import { Button, TextArea, TextField } from "@radix-ui/themes";
-import React from "react";
+import React, { useState } from "react";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
+interface IssueForm {
+  title: string;
+  description: string;
+}
 const NewIssuePage = () => {
+  const router = useRouter();
+  const [issueData, setIssueData] = useState<IssueForm>({
+    title: "",
+    description: "",
+  });
+  //   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+  //     const { name, value } = e.currentTarget;
+  //     setIssueData({
+  //       ...issueData,
+  //       [name]: value,
+  //     });
+  //   };
+  const handleTitleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    setIssueData({
+      ...issueData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const response = await axios.post("/api/issues", issueData);
+    router.push("/issues");
+  };
+  const handleDescriptionChange = (value: string) => {
+    setIssueData({
+      ...issueData,
+      description: value,
+    });
+  };
   return (
-    <div className="max-w-xl px-8 py-8">
+    <form className="max-w-xl px-8 py-8" onSubmit={handleSubmit}>
       <TextField.Root className="mb-6">
-        <TextField.Input placeholder="Title" />
+        <TextField.Input
+          placeholder="Title"
+          onChange={handleTitleChange}
+          name="title"
+        />
       </TextField.Root>
-      <TextArea
+      <SimpleMDE
         placeholder="Description of Issue"
-        size={"3"}
-        variant="surface"
+        onChange={handleDescriptionChange}
         className="mb-8"
       />
       <Button className="mt-9">Submit</Button>
-    </div>
+    </form>
   );
 };
 
