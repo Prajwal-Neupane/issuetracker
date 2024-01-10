1970e6309f24fba46c16132de5d2be7e83adc175
Prajwal Neupane <prajwalneupane.981@gmail.com>
2024-01-10 11:45:10 +0545

    styled the active list

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,11 +1,18 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
+// ... (imports and usePathname)
+
 const Navbar = () => {
+  const currentPathName = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+
   return (
     <nav className="flex gap-12 px-8 h-16 border-b items-center">
       <Link className="text-3xl" href={"/"}>
@@ -15,7 +22,12 @@ const Navbar = () => {
         {links.map((link) => (
           <li
             key={link.href}
-            className="text-xl text-zinc-700 hover:text-zinc-900 transition-colors"
+            className={`${
+              link.href === currentPathName ||
+              link.href === `${currentPathName}/`
+                ? "text-zinc-950"
+                : "text-zinc-600"
+            } text-xl hover:text-zinc-900 transition-colors`}
           >
             <Link href={link.href}>{link.label}</Link>
           </li>
